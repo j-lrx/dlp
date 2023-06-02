e7f804211e36481c7c159c7104bd2c397dd95e18
j-lrx <j.le-roux@groupeonepoint.com>
2023-06-02 14:32:18 +0200

    dev en cours

--- deidentification-job-trigger.tf
+++ deidentification-job-trigger.tf
@@ -10,7 +10,7 @@ resource "google_data_loss_prevention_job_trigger" "default" {
     }
 
     inspect_job {
-        inspect_template_name = "fake" #google_data_loss_prevention_inspect_template.default.id
+        inspect_template_name = google_data_loss_prevention_inspect_template.default.id
         
         actions {
             deidentify {
