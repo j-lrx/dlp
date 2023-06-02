b20c98e13846bd1facecf762918e2c0a19a6302d
j-lrx <j.le-roux@groupeonepoint.com>
2023-06-02 16:36:11 +0200

    dev en cours

--- deidentification-template.tf
+++ deidentification-template.tf
@@ -5,55 +5,64 @@ resource "google_data_loss_prevention_deidentify_template" "default" {
 
     deidentify_config {
 
-      record_transformations {
+      dynamic "record_transformations" {
+        for_each = var.column_to_mask != null || var.column_to_mask != null ? [1] : [0]
+        content {
 
-        ###################################################
-        #              Masquage de colonne                #
-        ###################################################
-        dynamic "field_transformations" {
-          for_each = var.column_to_mask != null ? [1] : [0]
-          content {
+          ###################################################
+          #              Masquage de colonne                #
+          ###################################################
+          dynamic "field_transformations" {
+            for_each = var.column_to_mask != null ? [1] : [0]
+            content {
 
-            dynamic fields {
-              for_each = var.column_to_mask != null ? [length(var.column_to_mask)] : [0]
-              content {
-                name = fields.value
-              }
-            }
-
-            primitive_transformation {
-              character_mask_config {
-                masking_character = "*"
-              } 
-            }
-          }
-        }
-
-        ###################################################
-        #     Anonymisation table par crypto réversible   #
-        ###################################################
-        dynamic "field_transformations" {
-          for_each = var.column_to_mask != null ? [1] : [0]
-          content {
-
-            dynamic fields {
-              for_each = var.column_to_crypto_deterministic != null ? [length(var.column_to_crypto_deterministic)] : [0]
-              content {
-                name = fields.value
-              }
-            }
-
-            primitive_transformation {
-              crypto_deterministic_config {
-                crypto_key {
-                  transient {
-                    name = "key-${random_integer.random_int.result}"
-                  }
+              dynamic fields {
+                for_each = var.column_to_mask != null ? [length(var.column_to_mask)] : [0]
+                content {
+                  name = fields.value
                 }
-              }          
+              }
+
+              primitive_transformation {
+                character_mask_config {
+                  masking_character = "*"
+                } 
+              }
+            }
+          }
+
+          ###################################################
+          #     Anonymisation table par crypto réversible   #
+          ###################################################
+          dynamic "field_transformations" {
+            for_each = var.column_to_mask != null ? [1] : [0]
+            content {
+
+              dynamic fields {
+                for_each = var.column_to_crypto_deterministic != null ? [length(var.column_to_crypto_deterministic)] : [0]
+                content {
+                  name = fields.value
+                }
+              }
+
+              primitive_transformation {
+                crypto_deterministic_config {
+                  crypto_key {
+                    transient {
+                      name = "key-${random_integer.random_int.result}"
+                    }
+                  }
+                }          
+              }
             }
           }
         }
+        ###################################################
+        #        Remplacemet texte par info_type          #
+        ###################################################
+
+
+
       }
     }
 }
