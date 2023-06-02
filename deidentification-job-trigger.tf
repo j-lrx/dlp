e54f714300ee93eea35b4396aac26c5ce9b1c2ba
j-lrx <j.le-roux@groupeonepoint.com>
2023-06-02 12:11:15 +0200

    dev en cours

--- deidentification-job-trigger.tf
+++ deidentification-job-trigger.tf
@@ -27,7 +27,7 @@ resource "google_data_loss_prevention_job_trigger" "default" {
 
             timespan_config {
                 start_time                                  = var.start_time
-                end_time                                    = var.end_time
+                #end_time                                    = var.end_time
                 enable_auto_population_of_timespan_config   = var.start_time == null && var.end_time == null ? true : false
                 timestamp_field {
                   name = ""
