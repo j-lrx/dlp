d7725b2aa7e729c78d20ebdd904eb7f82e9bcbbd
j-lrx <j.le-roux@groupeonepoint.com>
2023-06-02 08:10:41 +0200

    fix

--- side.tf
+++ side.tf
@@ -12,9 +12,6 @@ resource "google_storage_bucket" "output_bucket" {
 
 resource "google_storage_bucket_object" "table" {
   name    = "table.csv"
-  content ="Prénom,Nom,Téléphone,société
-  Julien,Le Roux,06 12 34 56 67,onepoint
-  Yann,Mitreau,07 12 34 56 67,système U
-  Florian,Grignon,06 76 65 43 32,indep"
+  content ="Prénom,Nom,Téléphone,société\nJulien,Le Roux,06 12 34 56 67,onepoint\nYann,Mitreau,07 12 34 56 67,système U\nFlorian,Grignon,06 76 65 43 32,indep"
   bucket  = google_storage_bucket.input_bucket.name
 }
