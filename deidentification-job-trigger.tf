7bdaf62fbceb88f30964a349cc094f23de4167e0
julien Le Roux <j.le-roux@groupeonepoint.com>
2023-06-15 14:15:08 +0200

    dev en cours

--- deidentification-job-trigger.tf
+++ deidentification-job-trigger.tf
@@ -18,7 +18,7 @@ resource "google_data_loss_prevention_job_trigger" "default" {
         file_types_to_transform = var.file_types_to_transform
 
         transformation_config {
-          structured_deidentify_template = count var.input_source_type == "TABLE" ? google_data_loss_prevention_deidentify_template.table.id : google_data_loss_prevention_deidentify_template.texte.id
+          structured_deidentify_template = google_data_loss_prevention_deidentify_template.table.id
         }
       } 
     }
